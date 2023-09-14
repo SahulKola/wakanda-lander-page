09be0aa68e00889d840006cc09e12e8c27dfb8c4
SahulKola <Mail2sahul.js@gmail.com>
2023-09-15 00:19:47 +0530

    Added Module files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgIconsModule } from '@ng-icons/core';
+import {
+  remixYoutubeFill,
+  remixTwitterFill,
+  remixDribbbleFill,
+  remixInstagramLine,
+} from '@ng-icons/remixicon';
+import { bootstrapSend } from '@ng-icons/bootstrap-icons';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HeroComponent } from './hero/hero.component';
+import { FeatureComponent } from './feature/feature.component';
+import { FooterComponent } from './footer/footer.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeroComponent,
+    FeatureComponent,
+    FooterComponent,
+    TestimonialComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgIconsModule.withIcons({
+      remixYoutubeFill,
+      remixTwitterFill,
+      remixDribbbleFill,
+      remixInstagramLine,
+      bootstrapSend,
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
